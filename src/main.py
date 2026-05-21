9b78683c7d2ba0e5537b8521ca02ebb847d5cf3d
reinhardbuyabo <buyabor@gmail.com>
2026-05-21 13:37:34 +0300

    milestone

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from os.path import isdir, isfile
 
 from copystatic import copy_files_recursive
 from markdown_blocks import markdown_to_blocks, markdown_to_html_node
@@ -16,13 +17,12 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    generate_page("content/index.md", "template.html", "public/index.html")
+    # generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 def extract_title(markdown):
     blocks = markdown_to_blocks(markdown)
-    if len(blocks) > 0:
-        print(blocks[0].split()[1])
     return blocks[0].split()[1]
 
 
@@ -41,8 +41,27 @@ def generate_page(from_path, template_path, dest_path):
     replaced = template_file.replace("{{ Title }}", title)
     result = replaced.replace("{{ Content }}", html_str)
 
-    with open(dest_path, 'w') as f:
+    with open(dest_path, "w") as f:
         f.write(result)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        full_src_path = os.path.join(dir_path_content, filename)
+        full_dest_path = (
+            os.path.join(dest_dir_path, filename.replace(".md", ".html"))
+            if filename.endswith(".md")
+            else os.path.join(dest_dir_path, filename)
+        )
+
+        if os.path.isfile(full_src_path):
+            if os.path.isdir(dest_dir_path):
+                generate_page(full_src_path, template_path, full_dest_path)
+            else:
+                os.makedirs(dest_dir_path)
+                generate_page(full_src_path, template_path, full_dest_path)
+        else:
+            generate_pages_recursive(full_src_path, template_path, full_dest_path)
 
 
 main()
